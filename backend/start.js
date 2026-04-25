b896e246d49890e42259c8dab5272ca3d14c9561
Chasmet <160381695+Chasmet@users.noreply.github.com>
2026-04-25 17:12:20 +0200

    Allow render media scale up to 5 minutes

--- backend/start.js
+++ backend/start.js
@@ -1,3 +1,3 @@
-process.env.MAX_RENDER_MEDIA = process.env.MAX_RENDER_MEDIA || "20";
+process.env.MAX_RENDER_MEDIA = process.env.MAX_RENDER_MEDIA || "30";
 await import("./ffmpeg-stability.js");
 await import("./server.js");
